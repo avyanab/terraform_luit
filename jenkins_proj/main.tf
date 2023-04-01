b8fbb4dbb5e0f4d3c681ea4848764d40d2586d44
avyanab <avyanab@gmail.com>
2023-04-01 17:39:28 +0000

    aws ec2 instance project

---
+++ jenkins_proj/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#security group: assign security group to jenkins ec2 for traffic on port 22 from your IP & traffic from port 8080
+resource "aws_security_group" "jenkins-ssh" {
+  name        = "jenkins-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-04f12000e3bbb67f3"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "jenkins-server" {
+  ami                    = "ami-04581fbf744a7d11f"
+  instance_type          = "t2.micro"
+  key_name               = "luitproject"
+  vpc_security_group_ids = [aws_security_group.jenkins-ssh.id]
+  subnet_id              = "subnet-0bd8cc27db41bea3b"
+  user_data              = file("jenkins.sh")
+}
+
+resource "aws_s3_bucket" "my-jenkins-b-2023" {
+  bucket = "my-jenkins-b-2023"
+}
+
+resource "aws_s3_bucket_acl" "my-jenkins-b-2023" {
+  bucket = aws_s3_bucket.my-jenkins-b-2023.id
+  acl    = "private"
+}
