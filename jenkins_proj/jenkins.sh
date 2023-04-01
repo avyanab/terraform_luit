41290bd0722c94a2c5a5ff02b17d3a982aea96f0
avyanab <avyanab@gmail.com>
2023-04-01 17:38:25 +0000

    to install jenkins

---
+++ jenkins_proj/jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#echo "Installing and starting Jenkins"
+#sudo yum update -y &&
+#sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo 
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#sudo yum upgrade -y &&
+#sudo amazon-linux-extras install java-openjdk11 -y
+#sudo yum install jenkins -y &&
+#sudo systemctl daemon-reload
+#sudo systemctl enable jenkins
+#sudo systemctl start jenkins
+#sudo systemctl status jenkins
