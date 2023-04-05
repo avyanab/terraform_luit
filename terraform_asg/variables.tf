f2d0474fdffdc182836d94f9e48d53e8070b16e9
avyanab <avyanab@gmail.com>
2023-04-05 01:55:29 +0000

    setting up asg project variables

---
+++ terraform_asg/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "default"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = {
+    "subnet-private1-us-east-1a" = 1
+    "subnet-private2-us-east-1b" = 2
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "subnet-public1-us-east-1a" = 1
+    "subnet-public2-us-east-1b" = 2
+  }
+}
+
