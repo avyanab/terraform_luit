8b0439f65781c7264240d7f1c5fead96749b6d58
avyanab <avyanab@gmail.com>
2023-04-06 01:43:45 +0000

    added subnet ids and names

--- terraform_asg/variables.tf
+++ terraform_asg/variables.tf
@@ -2,27 +2,29 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
+#add default VPC ID
 variable "default_vpc" {
   type    = string
-  default = "vpc-123456789"
+  default = "vpc-123456"
 }
 
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
-
-variable "private_subnets" {
-  default = {
-    "subnet-private1-us-east-1a" = 1
-    "subnet-private2-us-east-1b" = 2
-  }
+#add subnet IDs for public and private subnets in VPC
+variable "subnet-private1-us-east-1a" {
+  default = "subnet-123456"
 }
 
-variable "public_subnets" {
-  default = {
-    "subnet-public1-us-east-1a" = 1
-    "subnet-public2-us-east-1b" = 2
-  }
+variable "subnet-private2-us-east-1b" {
+  default = "subnet-123456"
+}
+
+variable "subnet-public1-us-east-1a" {
+  default = "subnet-123456"
+}
+
+variable "subnet-public2-us-east-1b" {
+  default = "subnet-123456"
 }
