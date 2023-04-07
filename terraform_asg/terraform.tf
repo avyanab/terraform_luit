15d803fd1d78f69c1319074a3ea8c313cf4a9dd9
avyanab <avyanab@gmail.com>
2023-04-07 02:18:01 +0000

    added s3 as backend w/ dynamodb table

--- terraform_asg/terraform.tf
+++ terraform_asg/terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~>4.0"
     }
   }
+  backend "s3" {
+    bucket         = "my-tf-asg-2023"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "my-tf-dynamodb-2023"
+  }
 }
