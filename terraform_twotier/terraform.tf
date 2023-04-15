06ab7a0e669ff128a91f135c403baab9cba94a3e
avyanab <avyanab@gmail.com>
2023-04-15 17:21:00 +0000

    added required tf version

--- terraform_twotier/terraform.tf
+++ terraform_twotier/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = "~> 1.4.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.62.0"
+      version = "~> 4.62.0"
     }
   }
 }
